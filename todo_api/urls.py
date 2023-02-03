2330249bec6e2354afc66cb3a669cb4094e61ed6
kaline <mesquitabfkalinekaline@gmail.com>
2023-02-03 13:21:42 -0300

    add details api view

--- todo_api/urls.py
+++ todo_api/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 #from . import views
 from .views import (
     TodoListApiView,
+    TodoDetailApiView
 )
 
 
 urlpatterns = [
 #path('', views.todos, name = 'todos'),
 path('api', TodoListApiView.as_view()),
-
+path('api/<int:todo_id>/', TodoDetailApiView.as_view()),
 ]
